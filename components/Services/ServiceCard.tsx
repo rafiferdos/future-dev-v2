f9ee8c057da40d9ebb801ad83d20a2bb41e44d51
mstsurnalyakter <surnalyms@gmail.com>
2024-11-28 16:57:27 +0600

    updated: service section

---
+++ components/Services/ServiceCard.tsx
@@ -0,0 +1,48 @@
+"use client";
+import React from "react";
+import { Card, CardHeader, CardBody } from "@nextui-org/card";
+
+
+import "@smastrom/react-rating/style.css";
+
+import { Avatar } from "@nextui-org/avatar";
+import Link from "next/link";
+
+// Define the type for the user prop
+interface Data {
+  id: string;
+  image: string;
+  title: string;
+  description: string;
+  link: string;
+}
+
+
+
+const ServiceCard: React.FC<{ data: Data }> = ({ data }) => {
+  const { image, title,  description,link } = data || {};
+
+  return (
+    <div className="w-full">
+      <Card className="p-5 border border-primary">
+        <CardHeader className="flex justify-between">
+          <div className="flex items-center gap-4">
+            <Avatar
+              isBordered
+              color="primary"
+              src={image}
+              className="w-20 h-20 text-large"
+            />
+          </div>
+        </CardHeader>
+        <CardBody className="space-y-3">
+          <h3 className="text-primary font-bold text-4xl">{title}</h3>
+          <p>{description}</p>
+          <Link className="font-bold" href={"#"}>{link}</Link>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
+
+export default ServiceCard;
---
+++ components/Services/ServicesSection.tsx
@@ -0,0 +1,71 @@
+
+
+import { Button } from "@nextui-org/button";
+import React from "react";
+import service from "@/public/service.png";
+import Image from "next/image";
+import servicesData from "../../lib/servicesData";
+import ServiceCard from "./ServiceCard";
+// Define a type
+type ServiceData = {
+  id: string;
+  image: string;
+  title:string,
+  description: string;
+  link:string
+};
+
+const ServicesSection: React.FC = () => {
+  return (
+    <div>
+      <div className="grid grid-cols-1 mt-10 md:grid-cols-2 lg:grid-cols-5 gap-24">
+        {/* Left Section */}
+        <div className="relative col-span-1 lg:col-span-2">
+          <div className="bg-primary w-[269px] min-h-96 rounded-r-[53px]"></div>
+          <div className="absolute bottom-10">
+            <Image
+              className="max-w-96"
+              src={service}
+              alt="Program Illustration"
+            />
+          </div>
+        </div>
+
+        {/* Right Section */}
+        <div className="flex lg:col-span-3 pl-6 space-y-10 lg:space-y-0 md:px-0 col-span-1 flex-col justify-center ">
+          <h2 className="font-bold text-3xl lg:text-5xl">
+            Explore Our Services
+          </h2>
+          <div className="space-y-1  lg:text-xl">
+            <p>
+              Discover diverse educational services designed to guide learners
+              toward academic excellence and personal growth.
+            </p>
+          </div>
+          <div className="flex gap-4 items-center">
+            <Button color="primary" variant="ghost">
+              Contact us
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      {/* card section */}
+      <div>
+        <div className="grid md:grid-cols-2 grid-cols-1  lg:grid-cols-3 gap-5 mt-28">
+          {servicesData?.length > 0 &&
+            servicesData.map((data: ServiceData, index: number) => (
+              <ServiceCard data={data} key={index} />
+            ))}
+        </div>
+        <div className="flex items-center justify-center mt-10">
+          <Button color="primary" variant="ghost">
+            Read More
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ServicesSection;
